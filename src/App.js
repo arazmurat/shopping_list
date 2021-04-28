375d989c22a91da2444f3dace84c99df75efadb1
Arazmyrat <arazmirat@icloud.com>
2021-04-28 23:40:08 +0400

    value

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   ]);
 
   const [inputValue, setInputValue] = useState("");
-  const [totalItemCount, setTotalItemCount] = useState(6);
+  const [totalItemCount, setTotalItemCount] = useState(0);
 
   const handleAddButtonClick = () => {
     const newItem = {
